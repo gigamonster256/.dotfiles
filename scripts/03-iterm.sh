5c307daad6ff91547a32b69786761f72e7a92c24
Caleb Norton <n0603919@outlook.com>
2024-04-15 16:25:49 -0500

    Use Nerd Font

--- scripts/03-iterm.sh
+++ scripts/03-iterm.sh
@@ -3,3 +3,9 @@ if ! grep -q "com.googlecode.iterm2" $ZDOTDIR/.zshrc.before; then
   echo defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$ZDOTDIR/iTerm/settings" >> $ZDOTDIR/.zshrc.before
   echo defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true >> $ZDOTDIR/.zshrc.before
 fi
+
+# install nerdfonts
+brew update
+brew tap homebrew/cask-fonts
+brew update
+brew install -q font-hack-nerd-font
