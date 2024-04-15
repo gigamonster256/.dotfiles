9e2d5ce1e07b444f368e3b31162ce8859fef278e
Caleb Norton <n0603919@outlook.com>
2024-04-15 16:38:46 -0500

    Set repos to use ssh after initial clone

--- scripts/04-git.sh
+++ scripts/04-git.sh
@@ -3,5 +3,7 @@ git config --global user.name "Caleb Norton"
 git config --global user.email "n0603919@outlook.com"
 
 # change dotfiles repo to use ssh (gpg should be setup by now)
-cd $HOME/.dotfiles
+cd $ZDOTDIR
 git remote set-url origin git@github.com:gigamonster256/.dotfiles.git
+cd $ZDOTDIR/nvim/
+git remote set-url origin git@github.com:gigamonster256/kickstart.nvim.git
