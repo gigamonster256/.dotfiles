3de76770cb97c1a942e9667bd7a588f0dc2b753f
Caleb Norton <n0603919@outlook.com>
2024-04-15 08:46:20 -0500

    Auto setup git name and email

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ if [ ! -d "$HOME/.dotfiles/.git" ]; then
   git remote add origin git@github.com:gigamonster256/.dotfiles.git
   git fetch origin
   git checkout -f -b main --track origin/main
+  git config --global user.name "Caleb Norton"
+  git config --global user.email "n0603919@outlook.com"
 fi
 
 # make sure submodules are inited
