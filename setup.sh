d1825b9d7f8733d12d51c3e550f3b87e5988c82f
Caleb Norton <n0603919@outlook.com>
2024-04-15 08:43:57 -0500

    Create setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# point zsh to use this directory for .zsh files
+if [ ! -f "$HOME/.zshenv" ]; then
+  ln -s $HOME/.dotfiles/.zshenv $HOME/.zshenv
+fi
+
+# set up git tracking if we downoaded dotfiles as a zip (no ssh login while setting up)
+if [ ! -d "$HOME/.dotfiles/.git" ]; then
+  git init
+  git remote add origin git@github.com:gigamonster256/.dotfiles.git
+  git fetch origin
+  git checkout -f -b main --track origin/main
+fi
+
+# make sure submodules are inited
+git submodule update --init --recursive
