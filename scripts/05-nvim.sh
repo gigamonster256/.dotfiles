6ee73f3e0c42e056b05386145d4c875876e72fe9
Caleb Norton <n0603919@outlook.com>
2024-04-16 13:59:13 -0500

    set default editor to neovim

--- scripts/05-nvim.sh
+++ scripts/05-nvim.sh
@@ -1,1 +1,11 @@
 brew update && brew install -q nvim ripgrep
+
+# alias vim to nvim
+if ! grep -q "alias vim=nvim" $ZDOTDIR/.zshrc.after; then
+  echo "alias vim=nvim" >> $ZDOTDIR/.zshrc.after
+fi
+
+# set editor to nvim
+if ! grep -q "export EDITOR=nvim" $ZDOTDIR/.zshrc.after; then
+  echo "export EDITOR=nvim" >> $ZDOTDIR/.zshrc.after
+fi
